5688465a370a04da5e4aa607d56667221e9e8d50
Matheus Lima <noutzinho@outlook.com>
2024-12-20 20:30:23 -0300

    Delete sim directory

--- sim/java/stringgen/src/main/java/com/stringgen/domain/Validations.java
+++
@@ -1,9 +0,0 @@
-package com.stringgen.domain;
-
-//Provides validation methods, such as checking the length of the string.
-public class Validations {
-    // Checks if the requested length is between 1 and 1000.
-    public static boolean isValidLength(int length) {
-        return length > 0 && length <= 1000;
-    }
-}
